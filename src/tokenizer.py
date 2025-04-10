456e823c26e8f300a0e0afbe19b528780fda65b2
mskim <mskim@casys.kaist.ac.kr>
2025-04-10 07:12:42 +0000

    use local tokenizer

--- src/tokenizer.py
+++ src/tokenizer.py
@@ -1,3 +1,5 @@
+from src.model import get_model_path
+
 def get_tokenizer(model: str, size: str):
     if model == "gpt2":
         return get_gpt2_tokenizer()
@@ -18,16 +20,16 @@ def get_gpt2_tokenizer():
 
 def get_opt_tokenizer(size: str):
     from transformers import AutoTokenizer
-    return AutoTokenizer.from_pretrained(f"facebook/opt-{size}", use_fast=False)
+    return AutoTokenizer.from_pretrained(get_model_path("opt", size), use_fast=False)
 
 def get_llama_tokenizer(size: str):
     from transformers import AutoTokenizer
-    return AutoTokenizer.from_pretrained(f"meta-llama/llama-2-{size}-hf", use_fast=False)
+    return AutoTokenizer.from_pretrained(get_model_path("llama2", size), use_fast=False)
 
 def get_mistral_tokenizer(size: str):
     from transformers import AutoTokenizer
-    return AutoTokenizer.from_pretrained(f"mistralai/Mistral-7B-v0.3", use_fast=False)
+    return AutoTokenizer.from_pretrained(get_model_path("mistral", size), use_fast=False)
 
 def get_mixtral_tokenizer(size: str):
     from transformers import AutoTokenizer
-    return AutoTokenizer.from_pretrained(f"mistralai/Mixtral-8x7B-v0.1", use_fast=False)
+    return AutoTokenizer.from_pretrained(get_model_path("mixtral", size), use_fast=False)
